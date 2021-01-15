f61eae65aace6fb95e616c4152e73ae894d9d1bc
Nader <icedemonslayerx@gmail.com>
2021-01-15 22:49:36 +0200

    rwert

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const app= require('./app');
 //const url='mongodb+srv://osama:53164299@cluster0.vsvsp.mongodb.net/guc?retryWrites=true&w=majority';
 const url='mongodb+srv://advancedLab35:advancedLab35@cluster0.a967h.mongodb.net/advancedLab35?retryWrites=true&w=majority'
 app.use(cors());
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    next();
+});
+
 mongoose.connect(url,
     { useNewUrlParser: true, useUnifiedTopology: true })
 .then(console.log('Successfully Connected to The Database'));
