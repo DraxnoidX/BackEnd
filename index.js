7502507affc8c018d3d07c2aeb20592d05129257
Nader <icedemonslayerx@gmail.com>
2021-01-15 23:51:17 +0200

    fhfrtg

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 let express = require('express');
 let mongoose= require('mongoose');
+
 //let bcrypt = require('bcrypt');
 const app= require('./app');
 
 //const url='mongodb+srv://osama:53164299@cluster0.vsvsp.mongodb.net/guc?retryWrites=true&w=majority';
 const url='mongodb+srv://advancedLab35:advancedLab35@cluster0.a967h.mongodb.net/advancedLab35?retryWrites=true&w=majority'
+const cors = require('cors')
+app.use(cors());
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    next();
+});
 
 mongoose.connect(url,
     { useNewUrlParser: true, useUnifiedTopology: true })
 .then(console.log('Successfully Connected to The Database'));
 
-
-app.listen(8080,() => console.log('API is running on http://localhost:8080/login'));
+var port = process.env.PORT || 3000; 
+app.listen(port);
