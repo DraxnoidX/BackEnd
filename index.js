b24caea557624306a0ceea10be11fad6eecb0b70
Nader <icedemonslayerx@gmail.com>
2021-01-15 22:45:16 +0200

    lkodsjnfoki

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let express = require('express');
 let mongoose= require('mongoose');
+let cors = require('cors');
 //let bcrypt = require('bcrypt');
 const app= require('./app');
 
 //const url='mongodb+srv://osama:53164299@cluster0.vsvsp.mongodb.net/guc?retryWrites=true&w=majority';
 const url='mongodb+srv://advancedLab35:advancedLab35@cluster0.a967h.mongodb.net/advancedLab35?retryWrites=true&w=majority'
-
+app.use(cors());
 mongoose.connect(url,
     { useNewUrlParser: true, useUnifiedTopology: true })
 .then(console.log('Successfully Connected to The Database'));
